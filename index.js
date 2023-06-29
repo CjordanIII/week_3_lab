6b51cb0dccab1e558af09867af19302bc5b57bf8
Clarence Jordan <clarencejordan087@gmail.com>
2023-06-29 14:58:14 -0700

    Get Even answered

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+// ================ get even ===========
+// goal: Write a for loop that will log only the even numbers in 0 through 200.
+
+const numOfnum = 200
+let even = 0
+
+for(let i = 0; i<=numOfnum; i++){
+    even = i%2
+    if(!even){
+        console.log(i)
+    }
+}
+
+
+
+
+
