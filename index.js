fac8d8836ff1f920e628a7626ebb29dbee7375d4
Clarence Jordan <clarencejordan087@gmail.com>
2023-06-30 07:09:17 -0700

    Fizzbuzz and evens doen

--- index.js
+++ index.js
@@ -17,7 +17,28 @@ for(let i = 0; i<=numOfnum; i++){
     }
 }
 
+const numOfnum2 = 100
 
+for(let i=1; i<=numOfnum2; i++){
+    let divible3 = i%3
+    let divible5 = i%5
+    const Fizz = 'Fizz '
+    const Buzz = 'Buzz '
+    if(!divible3 && !divible5){
+        console.log(Fizz+Buzz+ i)
+        continue
+    }
+    if(!divible3){
+
+        console.log(Fizz + i)
+        
+    }
+    if(!divible5){
+        console.log(Buzz + i)
+    }
+
+
+}
 
 
 
