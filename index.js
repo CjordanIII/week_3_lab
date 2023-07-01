f27898caf9d051ff11c9ff56a51c1cfb391b53cb
Clarence Jordan <clarencejordan087@gmail.com>
2023-07-01 13:20:58 -0700

    Fizz Buzz answered

--- index.js
+++ index.js
@@ -7,38 +7,40 @@
 // ================ get even ===========
 // goal: Write a for loop that will log only the even numbers in 0 through 200.
 
-const numOfnum = 200
-let even = 0
+// const numOfnum = 200
+// let even = 0
 
-for(let i = 0; i<=numOfnum; i++){
-    even = i%2
-    if(!even){
-        console.log(i)
-    }
-}
+// for(let i = 0; i<=numOfnum; i++){
+//     even = i%2
+//     if(!even){
+//         console.log(i)
+//     }
+// }
 
+
+//  ============ FizzBuzz ============
 const numOfnum2 = 100
 
-for(let i=1; i<=numOfnum2; i++){
-    let divible3 = i%3
-    let divible5 = i%5
-    const Fizz = 'Fizz '
-    const Buzz = 'Buzz '
-    if(!divible3 && !divible5){
-        console.log(Fizz+Buzz+ i)
-        continue
-    }
-    if(!divible3){
+// for(let i=1; i<=numOfnum2; i++){
+//     let divible3 = i%3
+//     let divible5 = i%5
+//     const Fizz = 'Fizz '
+//     const Buzz = 'Buzz '
+//     if(!divible3 && !divible5){
+//         console.log(Fizz+Buzz+ i)
+//         continue
+//     }
+//     if(!divible3){
 
-        console.log(Fizz + i)
+//         console.log(Fizz + i)
         
-    }
-    if(!divible5){
-        console.log(Buzz + i)
-    }
+//     }
+//     if(!divible5){
+//         console.log(Buzz + i)
+//     }
 
 
-}
+// }
 
 
 
